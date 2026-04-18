ae5548c2a6222d6459aef6b77584d1215dc0be17
Firmansyah Wicaksono <firmansyahwicaksono30@gmail.com>
2026-04-18 11:40:14 +0700

    feat: add terraform cloud organization include credentials

--- provider.tf
+++ provider.tf
@@ -1,4 +1,24 @@
+terraform {
+  required_version = "1.14.8"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.28" # To satisfy the highest minimum requirement from VPC module subnets
+    }
+  }
+
+  cloud {
+    
+    organization = "GCP-Experimental"
+
+    workspaces {
+      name = "GKE-Experimental"
+    }
+  }
+}
 provider "google" {
-  project     = "my-project-id"
-  region      = "asia-southeast2"
+  project     = var.project_id
+  region      = var.region
+  credentials = file("credentials.json") # Update with the actual path to your credentials file
 }
